fa2d7a53843d6f5cbdf1d65bcfdcb28639ac255e
JDPrabasha <shad3s.of.m3@gmail.com>
2021-08-05 07:37:49 +0530

    Update Q2.scala

--- Activity 1/Q2.scala
+++ Activity 1/Q2.scala
@@ -6,14 +6,16 @@ object Q2 extends App{
   }
   def prime(p:Int,n:Int=2):Boolean= n match
   {case x if(x==p) => true
-  case x if GCD(p,x)>1 => false
-  case x => prime(p,x+1)
+    case x if GCD(p,x)>1 => false
+    case x => prime(p,x+1)
   }
 
   def primeSeq(n:Int):Unit= {
-    if (n > 2)  primeSeq(n-1)
-    if (prime(n)) println(n)
+    if (n > 2) {
+      primeSeq(n - 1)
+      if (prime(n - 1)) println(n - 1)
+    }
   }
-//doesn't work for 11
+  
 
 }
