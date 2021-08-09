251e218310c4e74fcf2598a2ed3c3e65fab2e9c2
JDPrabasha <shad3s.of.m3@gmail.com>
2021-08-09 10:04:02 +0530

    Update cipher.scala

--- Assignment - The Caesar Cipher/cipher.scala
+++ Assignment - The Caesar Cipher/cipher.scala
@@ -1,1 +1,16 @@
+object Cipher extends App{
+
+    val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    def encrypt=(c:Char,key:Int,a:String)=>
+      a((a.indexOf(c.toUpper)+key)%a.size)
+  
+    def decrypt=(c:Char,key:Int,a:String)=>
+      a((if(((a.indexOf(c.toUpper)-key)%a.size)>0) ((a.indexOf(c.toUpper)-key)%a.size) else ((a.size)+(a.indexOf(c.toUpper)-key))))
+  
+    def cipher=(algo:(Char,Int,String)=>
+      Char,s:String,key:Int,a:String)=>
+      s.map(algo(_,key,a))
+  
+}
 
