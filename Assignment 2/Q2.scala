484a50b9a017587c3496eedbe913e3f81ecbd16b
JDPrabasha <shad3s.of.m3@gmail.com>
2021-08-20 18:20:15 +0530

    Delete Assignment 2 directory

--- Assignment 2/Q2.scala
+++
@@ -1,7 +0,0 @@
-object Q2 extends App {
-  val x = new Rational(3, 4)
-  val y = new Rational(5, 8)
-  val z = new Rational(2, 7)
-
-  println(x - y - z)
-}
--- Assignment 2/Rational.scala
+++
@@ -1,8 +0,0 @@
-class Rational(x: Int, y: Int) {
-  def numer: Int = x
-  def denom: Int = y
-
-  def -(r: Rational) = new Rational(this.numer*r.denom - r.numer*this.denom, this.denom*r.denom)
-
-  override def toString: String = numer + "/" + denom
-}
